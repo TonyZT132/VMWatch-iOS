226a5ba1ca6ddb3dadf4e90c66aa71ebdb48297e
TonyZT132 <tuo.zhang@mail.utoronto.ca>
2016-11-02 23:20:33 -0400

    added sha1 encryption

--- App/vmwatch/NewEC2WatchTableViewController.swift
+++ App/vmwatch/NewEC2WatchTableViewController.swift
@@ -144,3 +144,15 @@ class NewEC2WatchTableViewController: UITableViewController {
         }
     }
 }
+
+extension String {
+    func sha1() -> String {
+        let data = self.data(using: String.Encoding.utf8)!
+        var digest = [UInt8](repeating: 0, count:Int(CC_SHA1_DIGEST_LENGTH))
+        data.withUnsafeBytes {
+            _ = CC_SHA1($0, CC_LONG(data.count), &digest)
+        }
+        let hexBytes = digest.map { String(format: "%02hhx", $0) }
+        return hexBytes.joined()
+    }
+}
