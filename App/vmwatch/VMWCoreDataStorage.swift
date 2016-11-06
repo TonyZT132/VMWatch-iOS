f245c1d68fcc659cb445a626fa2944985ab22692
Tuo Zhang <tuo.zhang@mail.utoronto.ca>
2016-11-06 10:52:42 -0500

    update attributes and added error handling

--- App/vmwatch/VMWCoreDataStorage.swift
+++ App/vmwatch/VMWCoreDataStorage.swift
@@ -10,30 +10,34 @@ import Foundation
 import CoreData
 
 
-internal class VMWHistoryStorage {
+internal class VMWEC2HistoryStorage {
     
     private let context = (UIApplication.shared.delegate as! AppDelegate).getContext()
     
-    public func storeEC2History (accessID: String, accessKey: String) {
-        let entity =  NSEntityDescription.entity(forEntityName: "History", in: self.context)
+    public func storeEC2History (accessID: String, accessKey: String, instanceID: String, region:String) throws {
+        let entity =  NSEntityDescription.entity(forEntityName: "History_EC2", in: self.context)
         
         let transc = NSManagedObject(entity: entity!, insertInto: context)
         
         //set the entity values
         transc.setValue(accessID, forKey: "access_id")
         transc.setValue(accessKey, forKey: "access_key")
+        transc.setValue(instanceID, forKey: "instance_id")
+        transc.setValue(region, forKey: "region")
+        transc.setValue(Date(), forKey: "date")
         
         //save the object
         do {
             try context.save()
-            print("saved!")
+            print("Save success")
         } catch let error as NSError  {
             print("Could not save \(error), \(error.userInfo)")
+            throw VMWEC2CoreDataStorageError.DatabaseStoreError
         }
     }
     
-    func getEC2History () {
-        let fetchRequest: NSFetchRequest<History> = History.fetchRequest()
+    public func getEC2History () throws -> [History_EC2] {
+        let fetchRequest: NSFetchRequest<History_EC2> = History_EC2.fetchRequest()
         
         do {
             let searchResults = try self.context.fetch(fetchRequest)
@@ -42,8 +46,27 @@ internal class VMWHistoryStorage {
             for trans in searchResults as [NSManagedObject] {
                 print("\(trans.value(forKey: "access_id"))")
             }
+            return searchResults
         } catch {
             print("Error with request: \(error)")
+            throw VMWEC2CoreDataStorageError.DatabaseFetchError
         }
     }
 }
+
+internal class VMWHistoryDate {
+    private func getCurrentDate() -> Date {
+        return Date()
+    }
+    
+    public func getHistoryDateRange(date:Date){
+
+    }
+}
+
+enum VMWEC2CoreDataStorageError: Error {
+    case DatabaseStoreError
+    case DatabaseFetchError
+}
+
+
