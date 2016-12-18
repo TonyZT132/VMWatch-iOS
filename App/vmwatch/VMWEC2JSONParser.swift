8f4ee551c9d4f5717c64a9301fa644b15d773eb0
TonyZT132 <tuo.zhang@mail.utoronto.ca>
2016-12-18 08:42:02 -0500

    update json parser for ec2

--- App/vmwatch/VMWEC2JSONParser.swift
+++ App/vmwatch/VMWEC2JSONParser.swift
@@ -28,16 +28,18 @@ internal class VMWEC2JSONParser {
     }
     
     public func getAverageData(category:String) throws -> Double {
-        if let dict = self.data as? NSDictionary {
-            if let datapointsArray = dict["Datapoints"] as? NSArray {
-                if(datapointsArray.count > 0){
-                    var dataValue:Double = 0
-                    for i in 0 ..< datapointsArray.count{
-                        if let dataObj = datapointsArray[i] as? NSDictionary {
-                            dataValue = dataValue + (dataObj[category]! as AnyObject).doubleValue
+        if(self.data != nil){
+            if let dict = self.data as? NSDictionary {
+                if let datapointsArray = dict["Datapoints"] as? NSArray {
+                    if(datapointsArray.count > 0){
+                        var dataValue:Double = 0
+                        for i in 0 ..< datapointsArray.count{
+                            if let dataObj = datapointsArray[i] as? NSDictionary {
+                                dataValue = dataValue + (dataObj[category]! as AnyObject).doubleValue
+                            }
                         }
+                        return (dataValue / Double(datapointsArray.count))
                     }
-                    return (dataValue / Double(datapointsArray.count))
                 }
             }
         }
@@ -45,21 +47,23 @@ internal class VMWEC2JSONParser {
     }
     
     public func getDataPointsArray(category:String) throws -> NSMutableArray {
-        let dataArr = NSMutableArray()
-        if let dict = self.data as? NSDictionary {
-            if let datapointsArray = dict["Datapoints"] as? NSArray {
-                if(datapointsArray.count > 0){
-                    for i in 0 ..< datapointsArray.count {
-                        if let dataObj = datapointsArray[i] as? NSDictionary {
-                            var dict = [String:AnyObject]()
-                            dict["data"] = dataObj[category]! as AnyObject
-                            dict["time"] = dataObj["Timestamp"]! as AnyObject
-                            dataArr.add(dict as NSDictionary)
-                        } else {
-                            throw VMWEC2JSONParserError.InvalidEC2JSONDataError
+        if(self.data != nil){
+            let dataArr = NSMutableArray()
+            if let dict = self.data as? NSDictionary {
+                if let datapointsArray = dict["Datapoints"] as? NSArray {
+                    if(datapointsArray.count > 0){
+                        for i in 0 ..< datapointsArray.count {
+                            if let dataObj = datapointsArray[i] as? NSDictionary {
+                                var dict = [String:AnyObject]()
+                                dict["data"] = dataObj[category]! as AnyObject
+                                dict["time"] = dataObj["Timestamp"]! as AnyObject
+                                dataArr.add(dict as NSDictionary)
+                            } else {
+                                throw VMWEC2JSONParserError.InvalidEC2JSONDataError
+                            }
                         }
+                        return dataArr
                     }
-                    return dataArr
                 }
             }
         }
