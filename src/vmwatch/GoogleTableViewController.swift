ff3866ad64451983d14b69e104b4949c191bd408
Yanrong Wang <yanrong.wang@mail.utoronto.ca>
2017-01-08 02:10:13 -0500

    add google storyboard

--- src/vmwatch/GoogleTableViewController.swift
+++ src/vmwatch/GoogleTableViewController.swift
@@ -29,12 +29,21 @@ class GoogleTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 3
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        switch section {
+        case 0:
+            return 3
+        case 1:
+            return 1
+        case 2:
+            return 1
+        default:
+            return 0
+        }
     }
 
     /*
